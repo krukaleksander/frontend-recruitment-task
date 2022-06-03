cb45625afe7931b6ca24acf226adfcb1c3eb302a
aleksander <kruk.aleksander@hotmail.com>
2022-06-03 13:39:04 +0200

    feat: use local storage to store actual value

--- src/js/main.js
+++ src/js/main.js
@@ -10,14 +10,18 @@
     value = 0;
     container = document.querySelector('.alert__count');
     resetButton = document.querySelector('.alert__btn');
+    localStorageActualValue = localStorage.getItem('taskStorage')
 
         init() {
             this.resetButton.addEventListener('click', () => this.resetValue())
+            if(!this.localStorageActualValue) return this.setLocalStorageValue('0');
+            this.value = localStorage.getItem('taskStorage')
         }
         click() {
             if(this.value >=4) this.displayResetButton()
             this.value++
             this.container.innerText = this.value
+            this.setLocalStorageValue(this.value.toString())
         }
         displayResetButton() {
             this.resetButton.style.display = 'inline-block';
@@ -26,6 +30,9 @@
             this.value = 0;
             this.container.innerText = this.value;
             this.resetButton.style.display = 'none';
+        }
+        setLocalStorageValue(value) {
+            localStorage.setItem('taskStorage', value)
         }
     }
 
